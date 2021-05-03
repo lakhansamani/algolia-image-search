caba87f35b2078c72e74ef80343fd2ede44745fe
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-05-03 16:20:40 +0530

    Resolves #8

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.setTimeout(30000);
+
+test('should search lazy_fox.png', async () => {
+	const { container } = render(<App />);
+	// add delay for response to be there
+	await new Promise((r) => setTimeout(r, 5000));
+	// get initial result items
+	const resultItemsInitially = container.getElementsByClassName('resultItem')
+		.length;
+	await new Promise((r) => setTimeout(r, 1000));
+	// find the algolia search element
+	const inputElement = container.getElementsByClassName(
+		'ais-SearchBox-input',
+	)[0];
+
+	// fire search event
+	fireEvent.change(inputElement, { target: { value: 'lazy_fox.png' } });
+	// add delay for response to be there
+	await new Promise((r) => setTimeout(r, 2000));
+	// get result items
+	const resultItems = container.getElementsByClassName('resultItem').length;
+	// we cannot compare the exact number of results, because if someone else uploads a file this result could change but can make sure that number of results after search are less than number of results before search
+	expect(resultItems).toBeLessThan(resultItemsInitially);
+	unmountComponentAtNode(container);
 });
