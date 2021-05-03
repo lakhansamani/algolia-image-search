17dbe0b18789934d1a6b99d8d8ac599c8292388a
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-05-03 08:40:58 +0530

    fix: add error handling to serverless function

--- functions/index_ocr_data.ts
+++ functions/index_ocr_data.ts
@@ -15,51 +15,78 @@ const handler: Handler = async (event, context) => {
 		};
 	}
 
-	const client = algoliasearch(
-		process.env.ALGOLIA_USER_ID,
-		process.env.ALGOLIA_KEY,
-	);
-	const index = client.initIndex('ocr_search');
-	const encoded = event.headers.authorization.split(' ')[1];
-	// decode it using base64
-	const decoded = new Buffer(encoded, 'base64').toString();
-	const username = decoded.split(':')[0];
-	const password = decoded.split(':')[1];
-
-	if (username !== process.env.USERNAME || password !== process.env.password) {
+	if (event.httpMethod !== 'POST') {
 		return {
-			statusCode: 401,
+			statusCode: 405,
 			body: JSON.stringify({
-				message: 'Unauthorized',
+				message: `${event.httpMethod} Method not allowed`,
 			}),
 		};
 	}
 
-	const data = JSON.parse(event.body);
+	try {
+		const client = algoliasearch(
+			process.env.ALGOLIA_USER_ID,
+			process.env.ALGOLIA_KEY,
+		);
+		const index = client.initIndex('ocr_search');
+		const encoded = event.headers.authorization.split(' ')[1];
+		// decode it using base64
+		const decoded = Buffer.from(encoded, 'base64').toString().split(':');
+		const username = decoded[0];
+		const password = decoded[1];
 
-	if (!data.text || !data.fileName || !data.fileType) {
+		console.log(`username: ${username}`);
+		console.log(`password: ${password}`);
+
+		if (
+			username !== process.env.USERNAME ||
+			password !== process.env.password
+		) {
+			return {
+				statusCode: 401,
+				body: JSON.stringify({
+					message: 'Unauthorized',
+				}),
+			};
+		}
+
+		const data = JSON.parse(event.body);
+
+		if (!data.text || !data.fileName || !data.fileType) {
+			return {
+				statusCode: 400,
+				body: JSON.stringify({
+					message: 'Invalid data',
+				}),
+			};
+		}
+
+		await index.saveObjects(
+			{
+				...data,
+				createdAt: new Date(),
+			},
+			{ autoGenerateObjectIDIfNotExist: true },
+		);
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: 'Data indexed successfully',
+			}),
+		};
+	} catch (err) {
+		console.error(err);
+
 		return {
 			statusCode: 500,
 			body: JSON.stringify({
-				message: 'Invalid data',
+				message: `Internal server error`,
+				error: err,
 			}),
 		};
 	}
-
-	await index.saveObjects(
-		{
-			...data,
-			createdAt: new Date(),
-		},
-		{ autoGenerateObjectIDIfNotExist: true },
-	);
-
-	return {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: 'Data indexed successfully',
-		}),
-	};
 };
 
 export { handler };
